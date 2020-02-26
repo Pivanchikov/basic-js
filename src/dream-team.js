81aa3e72eb4a33476dcdd3ddafd8ae67ec535114
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-26 18:51:30 +0300

    fix;  error using object

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,21 +1,23 @@
 module.exports = function createDreamTeam(array) {
       if (typeof(array) != typeof([])) {return false}
+      if (array.length == undefined) {return false}
       let members = [];
-  for (let i = 0; i < array.length; i++){
-        if (typeof(array[i]) == typeof("string")) {
-              for (let j = 0; j < array[i].length; j++) {
-                    if(array[i][j] != 0 ) {
-                        members.push(array[i][j]);
-                        break
-                    }
-              }
-        }
-  }
-  members = members.join("").toUpperCase();
-  let member = [];
-      for (let i = 0; i < members.length; i++) {
-            member.push(members[i]);
-              }
-            member = member.sort().join("");
-      return member;
+      for (let i = 0; i < array.length; i++){
+            if (typeof(array[i]) === typeof("string")) {
+                  for (let j = 0; j < array[i].length; j++) {
+                        if(array[i][j] != 0 ) {
+                              members.push(array[i][j]);
+                              break
+                        }
+                  }
+            }
+      }
+      members = members.join("").toUpperCase();
+      let member = [];
+            for (let i = 0; i < members.length; i++) {
+                  member.push(members[i]);
+                  }
+                  member = member.sort().join("");
+            return member;
 };
+
