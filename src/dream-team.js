97e1c537bd961f91a1c5547a902fbc4f7f074b37
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-26 18:33:19 +0300

    init: start algorithmic task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,21 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+      if (typeof(array) != typeof([])) {return false}
+      let members = [];
+  for (let i = 0; i < array.length; i++){
+        if (typeof(array[i]) == typeof("string")) {
+              for (let j = 0; j < array[i].length; j++) {
+                    if(array[i][j] != 0 ) {
+                        members.push(array[i][j]);
+                        break
+                    }
+              }
+        }
+  }
+  members = members.join("").toUpperCase();
+  let member = [];
+      for (let i = 0; i < members.length; i++) {
+            member.push(members[i]);
+              }
+            member = member.sort().join("");
+      return member;
 };
