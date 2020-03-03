6cc2c7a270ffc5901d6efc24439fcac17ebfd741
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-03-03 15:33:35 +0300

    fix: problem with letter

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -8,7 +8,11 @@ module.exports =function dateSample(sampleActivity) {
             if (sampleActivity <= 0 || sampleActivity > 15 ) {
                   return false;
             }
+            if (sampleActivity.match(/^[a-z]/i)){
+                  return false
+            }
             let k = 0.693/HALF_LIFE_PERIOD;
             let f = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
             return f
+            
       };
