edc4571e905e3aee9996912e6bedce24921e9de5
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-03-03 16:24:29 +0300

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports =function dateSample(sampleActivity) {
+            let i = 0;
             if (typeof(sampleActivity) != "string") {
                   return false;
             }
@@ -9,10 +10,14 @@ module.exports =function dateSample(sampleActivity) {
                   return false;
             }
             if (sampleActivity.match(/^[a-z]/i)){
-                  return false
+                  return false;
+            }
+            if (sampleActivity.match(/\d+\.\d+\./g)){
+                      i= sampleActivity.match(/^(\d+\.\d+)/);
+                      sampleActivity = i[0];
             }
             let k = 0.693/HALF_LIFE_PERIOD;
             let f = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
             return f
-            
+           
       };
