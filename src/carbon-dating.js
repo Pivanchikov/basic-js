25f0dc032efe7c37a55b35edca0a1aac71fc5017
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-03-03 13:44:05 +0300

    init: start algorithmic task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,14 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
-};
+module.exports =function dateSample(sampleActivity) {
+            if (typeof(sampleActivity) != "string") {
+                  return false;
+            }
+            if (sampleActivity <= 0 || sampleActivity > 15 ) {
+                  return false;
+            }
+            let k = 0.693/HALF_LIFE_PERIOD;
+            let f = Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivity)/k);
+            return f
+      };
