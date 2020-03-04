21ff7099165ae740319cd80b032b55182a021761
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-03-04 14:43:17 +0300

    init: start algorithmic task

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if(date === undefined) {
+        return 'Unable to determine the time of year!';
+  }
+  {try {date.getTime();} catch(e) {throw Error(e);}}
+        if (date.getMonth() === 11 || date.getMonth() === 0 || date.getMonth() === 1){
+               return "winter";
+        };
+        if (date.getMonth() > 1 && date.getMonth() < 5){
+              return  "spring";
+        };
+        if (date.getMonth() > 4 && date.getMonth() < 8){
+              return  "summer";
+        };
+        if (date.getMonth() > 7 && date.getMonth() < 11){
+              return "autumn";
+        };
+
+  
 };
