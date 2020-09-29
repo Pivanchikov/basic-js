9d846739b5bb0acf97033dce7c9cd932a0be32d9
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-09-29 15:26:55 +0300

    add: solution to extended-repeater;

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,33 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater (str, options) {
+    if (options.hasOwnProperty('addition')){
+         let addition = options.addition;
+          if (options.hasOwnProperty('additionRepeatTimes') && options.additionRepeatTimes !== undefined){
+            let length = 1
+            if (options.hasOwnProperty('additionSeparator')){
+              addition += options.additionSeparator;
+             length = options.additionSeparator.length
+            } else {
+              addition += '|'
+            }
+            addition = addition.repeat(options.additionRepeatTimes);
+            addition = addition.slice(0, -length)
+          }
+        str += addition;
+      }
+      let length = 1;
+  
+      if (options.hasOwnProperty('repeatTimes') && options.repeatTimes !== undefined){
+        if (options.hasOwnProperty('separator')){
+        str += options.separator
+        length = options.separator.length
+      } else {
+        str += '+'
+      }
+  
+        str = str.repeat(options.repeatTimes)
+        str = str.slice(0, -length)
+      }
+  
+      return str
+  };
   
